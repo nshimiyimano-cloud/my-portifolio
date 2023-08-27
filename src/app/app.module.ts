000610d4c42f787dba8f41461d5733eb7b91b859
nshimiyimano-cloud <njeanluc828@gmail.com>
2023-08-28 01:44:44 +0200

    created about,services,blog,certificates coponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ServicesComponent } from './services/services.component';
+import { CertificatesComponent } from './certificates/certificates.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactUsComponent,
     NavbarComponent,
+    AboutusComponent,
+    ServicesComponent,
+    CertificatesComponent,
+    BlogComponent,
   ],
   imports: [
     BrowserModule,
