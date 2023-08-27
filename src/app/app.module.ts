0606c0510e4b17a5e95bb7bd97110dd92e2ae1d8
nshimiyimano-cloud <njeanluc828@gmail.com>
2023-08-28 01:22:09 +0200

    created navbar component and link in main tempalte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactUsComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
