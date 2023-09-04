34f329ba29e306d5ea61bd911069744ec07525fa
nshimiyimano-cloud <njeanluc828@gmail.com>
2023-09-04 15:21:27 +0200

    make some files to be loaded on web and added experience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -34,7 +33,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     NgxTypedJsModule,
     BrowserAnimationsModule,
     MatIconModule,
